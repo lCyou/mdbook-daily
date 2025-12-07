b075ecb87422fb7a478c0ceb623520e803e9cfd0
K.Yamakawa <87867174+lCyou@users.noreply.github.com>
2025-12-07 23:46:44 +0900

    add: sidebar anchor

--- theme/custom.js
+++ theme/custom.js
@@ -8,3 +8,21 @@
       .top - 50
     );
 })();
+
+(function () {
+  var as = document.querySelectorAll('a.header')
+  var newList = document.createElement('ul');
+  newList.setAttribute('class', 'innerLink');
+  for (var i = 1, l = as.length; i < l; i += 1) {
+    var a = as[i];
+    var label = a.innerText;
+    var href = a.getAttribute('href');
+    var newAnchor = document.createElement('a');
+    newAnchor.setAttribute('href', href);
+    newAnchor.innerHTML = label;
+    var newItem = document.createElement('li');
+    newItem.appendChild(newAnchor);
+    newList.appendChild(newItem);
+  }
+  document.querySelector('.sidebar .active').appendChild(newList);
+})();
